547c50e4911f8ce4d42a865642b37ea8b66a39b6
Joshhortt <joshhortt@yahoo.com>
2021-01-18 20:01:06 +0000

    Alteração XML e adicionar metodo privado 'Sobrenome' para testar na Console.

---
+++ 13-POO-Publicas_Privadas/Classes/Cliente.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+	public class Cliente
+	{   
+		/// <summary>
+		/// construtor com 3 parametros
+		/// </summary>
+		/// <param name="nome"></param>
+		/// <param name="telefone"></param>
+		/// <param name="cc"></param>
+		public Cliente(string nome, string telefone, string cc)   		  
+		{
+			this.Nome = nome;
+			this.Telefone = telefone;
+			this.CC = cc;
+		}
+
+		/// <summary>
+		/// Criar Construtor para telefone com parametro 'int'. 
+		/// <param name="telefone">telefone inteiro</param>
+		public Cliente(int telefone)
+		{
+			this.Telefone = telefone.ToString();
+		}
+
+		/// <summary>
+		/// Construtor sem parametros
+		/// </summary>
+		public Cliente()  { }
+		
+		public static string Teste;
+		public string Nome;
+		public string Telefone;
+		public string CC;
+
+		private string Sobrenome = "Santos";
+
+		public void Gravar()
+		{
+			this.Olhar();
+			return;
+		
+			var clientes = Cliente.LerClientes();
+			clientes.Add(this);
+			if (File.Exists(caminhoBaseClientes()))
+			{
+				StreamWriter r = new StreamWriter(caminhoBaseClientes());
+				string conteudo = "nome;telefone;cc;";
+				r.WriteLine(conteudo);
+				foreach (Cliente c in clientes)
+				{
+					var linha = c.Nome + ";" + c.Telefone + ";" + c.CC + ";";
+					r.WriteLine(linha);
+				}
+				r.Close();  
+			}
+		}
+
+		private void Olhar() // Criar um metodo de instancia privado
+		{
+			Console.WriteLine("O cliente " + this.Nome + " " + this.Sobrenome + " está olhando para mim.");
+			Console.WriteLine("===================================================");
+		}
+
+		private static string caminhoBaseClientes()
+		{
+			return ConfigurationManager.AppSettings["BaseDeClientes"];
+		}
+
+		public static List<Cliente> LerClientes()
+		{
+			var clientes = new List<Cliente>();
+
+			if (File.Exists(caminhoBaseClientes()))
+			{
+				using (StreamReader arquivo = File.OpenText(caminhoBaseClientes()))
+				{
+					string linha;
+					int i = 0;
+					while ((linha = arquivo.ReadLine()) != null)
+					{
+						i++;
+						if (i == 1) continue;
+						var clienteArquivo = linha.Split(';');
+
+						var cliente = new Cliente(clienteArquivo[0], clienteArquivo[1], clienteArquivo[2]);
+
+						clientes.Add(cliente);
+					}
+				}
+			}
+
+				return clientes;
+		}
+	}
+}
+
---
+++ 13-POO-Publicas_Privadas/Program.cs
@@ -0,0 +1,56 @@
+﻿//**********************************************************************************************************************
+// 13. Programação orientada a Objectos - Construtores (Continuação da aula anterior)
+//**********************************************************************************************************************
+//Nesta aula iremos aprender a utilização dos atributos e métodos públicos e privados, você verá na prática como 
+//utilizá-lo e também iremos alterar a nossa aplicação console criando mais uma opção no menu.
+//**********************************************************************************************************************
+
+using Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tela;
+
+namespace _13_POO_Publicas_Privadas
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Menu.Criar();
+
+			Cliente.Teste = "sss";
+
+			var cliente = new Cliente();   
+			
+			cliente.Nome = "Geraldo";
+			cliente.Telefone = "911815870";
+			cliente.CC = "9690540";
+			cliente.Gravar();
+
+			var clientes = Cliente.LerClientes();
+			foreach (Cliente c in clientes) 
+			{
+				Console.WriteLine(c.Nome);
+				Console.WriteLine(c.Telefone);
+				Console.WriteLine(c.CC);
+				Console.WriteLine("=============================================");
+			}
+			Console.ReadLine();
+			var l = new List<String>();
+			
+			var cliente2 = new Cliente();
+			cliente2.Nome = "Rita";
+			cliente2.Telefone = "911815878";
+			cliente2.CC = "9690544";
+			cliente2.Gravar();
+			
+		}
+	}
+}
+
+
