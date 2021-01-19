f07c3c0c6db8c9d21dd69f64c9b4edd18fcdc40f
Joshhortt <joshhortt@yahoo.com>
2021-01-19 11:51:54 +0000

    Primeiro commit do projeto parte 14. Adicionar nova classe Usuario'

---
+++ 14-POO-Heranca/Funções/TelaCliente.cs
@@ -0,0 +1,63 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funções
+{
+	class TelaCliente
+	{
+		public static void Chamar()
+		{
+
+			Console.WriteLine("\n========================== Cadastro de Cliente ==========================\n");
+			while (true)
+			{
+
+				string mensagem = "Digite uma das opções abaixo:\n" +
+				"\n    0 - Sair do Cadastro" +
+				"\n    1 - Para cadastrar clientes" +
+				"\n    2 - Para listar clientes";
+
+				Console.WriteLine(mensagem);
+
+				int valor = int.Parse(Console.ReadLine());
+
+				if (valor == 0)
+				{
+					break;
+				}
+				else if (valor == 1)
+				{
+					// Ler para dentro do cliente
+					var cliente = new Cliente();
+					
+					Console.WriteLine("Digite o nome do cliente");
+					cliente.Nome = Console.ReadLine();  //"Geraldo";
+
+					Console.WriteLine("Digite o numero de Telefone do cliente");
+					cliente.Telefone = Console.ReadLine();  //"911815870";
+
+					Console.WriteLine("Digite o numero do CC do cliente");
+					cliente.CC = Console.ReadLine(); // "9690540";
+					cliente.Gravar();
+				}
+				else
+				{
+					var clientes = Cliente.LerClientes();
+					foreach (Cliente c in clientes)
+					{
+						Console.WriteLine(c.Nome);
+						Console.WriteLine(c.Telefone);
+						Console.WriteLine(c.CC);
+						Console.WriteLine("=============================================");
+					}
+				}
+
+			}
+		}
+	}
+}
+
---
+++ 14-POO-Heranca/Tela/Menu.cs
@@ -0,0 +1,75 @@
+﻿using Calculo;
+using Diretorio;
+using Funções;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tela
+{
+	class Menu
+	{
+		public const int SAIDA_PROGRAMA = 0;
+		public const int LER_ARQUIVOS = 1;
+		public const int TABUADA = 2;
+		public const int CALCULO_MEDIA = 3;
+		public const int CADASTRAR_CLIENTES = 4;  // Adiionar novo item de menu.
+
+
+		public static void Criar()
+		{
+			while (true)
+			{
+				string mensagem = "Olá Usuário! Benvindo ao programa,\n" +
+				"\n  utilizando a Progrmação Funcional\n" +
+				"\n\n   Digite uma das opções abaixo: \n" +
+				"\n    0 - Sair do Programa" +
+				"\n    1 - Para Ler Arquivos" +
+				"\n    2 - Para executar a Tabuada" +
+				"\n    3 - Para Calcular a Média dos Alunos" +
+				"\n    4 - Cadastrar Clientes";  // Novo item de menu. Opção 4 adicionado.
+
+				Console.WriteLine(mensagem);
+				
+				int valor = int.Parse(Console.ReadLine());
+
+				if (valor == SAIDA_PROGRAMA)
+				{
+					break;
+				}
+				else if (valor == LER_ARQUIVOS)
+				{
+					Console.WriteLine("\n========================== Opção Ler Arquivos ==========================\n");
+					Arquivo.Ler(1);
+					Console.WriteLine("\n============================================================================\n");
+				}
+				else if (valor == TABUADA)
+				{
+					Console.WriteLine("\n==================== Opção Tabuada =============================================\n");
+					Console.WriteLine("Digite o numero da Tabuada");
+					int numero = int.Parse(Console.ReadLine());
+					Tabuada.Calcular(numero);
+					Console.WriteLine("\n================================================================================\n");
+				}
+				else if (valor == CALCULO_MEDIA)
+				{
+					Console.WriteLine("\n==================== Opção Calculo da Media========================================\n");
+					Media.Alunos();			
+					Console.WriteLine("\n================================================================================\n");
+				}
+				else if (valor == CADASTRAR_CLIENTES)
+				{   // Adicionar condicional ao Cadastrar Clientes ao novo item de menu
+					// Console.WriteLine("\n==================== Opção Cadastrar Clientes========================================\n");
+					TelaCliente.Chamar();
+					Console.WriteLine("\n================================================================================\n");
+				}
+				else
+				{
+					Console.WriteLine("OPÇAO INVALIDA");
+				}
+			}
+		}
+	}
+}
