a8a744b4bbf45ecb21b56befc438f49e91f1feab
Joshhortt <joshhortt@yahoo.com>
2021-01-20 11:49:09 +0000

    Recolocar codigo comentado na Classe Cliente

--- 15-POO-Polimorfismo/Classes/Usuario.cs
+++ 15-POO-Polimorfismo/Classes/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,43 +18,21 @@ namespace Classes
 		public Usuario() { }
 
 		public void Gravar() 
-		{
-			/* Remover toda a condição if else daqui pois só é pretendido aqui salvar os dados de usuário em vez de cliente.
-			if (this.GetType() == typeof(Cliente))
-			{
-				var clientes = Cliente.LerClientes();
-				clientes.Add(this);
-				if (File.Exists(caminhoBaseClientes()))
 				{
-					StreamWriter r = new StreamWriter(caminhoBaseClientes());
-					string conteudo = "nome;telefone;cc;";
-					r.WriteLine(conteudo);
-					foreach (Cliente c in clientes)
-					{
-						var linha = c.Nome + ";" + c.Telefone + ";" + c.CC + ";";
-						r.WriteLine(linha);
-					}
-					r.Close();
-				}
-			}
-			else
+			var usuario = Usuario.LerUsuarios();
+			Usuario u = new Usuario(this.Nome, this.Telefone, this.CC);
+			usuario.Add(u);
+			if (File.Exists(caminhoBaseUsuarios()))
 			{
-			*/
-				var usuario = Usuario.LerUsuarios();
-				Usuario u = new Usuario(this.Nome, this.Telefone, this.CC);
-				usuario.Add(u);
-				if (File.Exists(caminhoBaseUsuarios()))
+				StreamWriter r = new StreamWriter(caminhoBaseUsuarios());
+				string conteudo = "nome;telefone;cc;";
+				r.WriteLine(conteudo);
+				foreach (Usuario c in usuario)
 				{
-					StreamWriter r = new StreamWriter(caminhoBaseUsuarios());
-					string conteudo = "nome;telefone;cc;";
-					r.WriteLine(conteudo);
-					foreach (Usuario c in usuario)
-					{
-						var linha = c.Nome + ";" + c.Telefone + ";" + c.CC + ";";
-						r.WriteLine(linha);
-					}
-					r.Close();
+					var linha = c.Nome + ";" + c.Telefone + ";" + c.CC + ";";
+					r.WriteLine(linha);
 				}
+				r.Close();
 			}
 		}
 	}
