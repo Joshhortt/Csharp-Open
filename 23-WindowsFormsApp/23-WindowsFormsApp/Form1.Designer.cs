fb772888fdcf26508e2cf6e4f9d190e762d80b67
Joshhortt <joshhortt@yahoo.com>
2021-02-01 13:33:33 +0000

    Primeiro commit da parte 23 do projeto WinForms.

---
+++ 23-WindowsFormsApp/23-WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace _23_WindowsFormsApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtResultado = new System.Windows.Forms.TextBox();
+			this.btnOk = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(34, 44);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(331, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Pressione OK para preencher a caixa de texto";
+			// 
+			// txtResultado
+			// 
+			this.txtResultado.Location = new System.Drawing.Point(38, 79);
+			this.txtResultado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtResultado.Name = "txtResultado";
+			this.txtResultado.Size = new System.Drawing.Size(327, 26);
+			this.txtResultado.TabIndex = 1;
+			// 
+			// btnOk
+			// 
+			this.btnOk.Location = new System.Drawing.Point(38, 125);
+			this.btnOk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(327, 35);
+			this.btnOk.TabIndex = 2;
+			this.btnOk.Text = "OK";
+			this.btnOk.UseVisualStyleBackColor = true;
+			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(927, 720);
+			this.Controls.Add(this.btnOk);
+			this.Controls.Add(this.txtResultado);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Form1";
+			this.Text = "Aplicação 1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtResultado;
+		private System.Windows.Forms.Button btnOk;
+	}
+}
+
