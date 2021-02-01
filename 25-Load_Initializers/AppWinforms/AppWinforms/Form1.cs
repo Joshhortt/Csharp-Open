5b854a69d9237325c52dc6e1903e2f10e1ebfa01
Joshhortt <joshhortt@yahoo.com>
2021-02-01 17:25:26 +0000

    Primeiro commit da parte 25 do projeto WinForms.

---
+++ 25-Load_Initializers/AppWinforms/AppWinforms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			txtResultado.Text = "Benvindos a nossa primeira aula de Winforms!";
+		}
+
+		private void btnAbre_Click(object sender, EventArgs e)
+		{
+			var form = new FrmShow();
+			form.lblMensagem.Text = txtResultado.Text;
+			form.Show();
+		
+		}
+	}
+}
---
+++ 25-Load_Initializers/AppWinforms/AppWinforms/Program.cs
@@ -0,0 +1,26 @@
+﻿//25. Load e Initializers
+//Nesta aula iremos aprender os eventos Load e Initializer de uma Aplicação Windows, 
+//iremos saber como os programas verificam se o acesso a aplicação é válida ou não.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
