e64fc93202e6ccc9017fa1423505f046f70485f5
Joshhortt <joshhortt@yahoo.com>
2021-02-01 14:48:05 +0000

    Primeiro commit da parte 24 do projeto WinForms.

---
+++ 24-Manipulado_Formularios/AppWinforms/AppWinforms/Program.cs
@@ -0,0 +1,26 @@
+﻿//24. Manipulação de Formularios
+//Nesta aula iremos aprender como manipular formulários de nossa Aplicação Windows, 
+//iremos notar nesta aula que os conceitos de orientação a objetos são essenciais.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
