675e6343e7c1d2e852572022cf935cecd4c7db20
Joshhortt <joshhortt@yahoo.com>
2021-02-02 13:17:52 +0000

    Preencher os dados no ComboBox de forma dinâmica.

--- 26-ComboBox/AppWinforms/AppWinforms/Form1.cs
+++ 26-ComboBox/AppWinforms/AppWinforms/Form1.cs
@@ -10,6 +10,49 @@ using System.Windows.Forms;
 
 namespace AppWinforms
 {
+	public class Estado
+	{
+		public int Id;     // propriedade Id do tipo int
+		public string Nome;   // propriedade Nome do tipo string
+
+		public override string ToString()  // override de APPWinforms Estado (fazendo com que nao apareça na textBox
+		{
+			return this.Nome; // retorna o Nome do Estado 
+		}
+
+		public static List<Estado> Lista()  // Criar Lista de Estados com numero Id e Nome
+		{
+			// SP
+			var lista = new List<Estado>();
+			var est1 = new Estado();
+			est1.Id = 1;
+			est1.Nome = "SP";
+
+			var s = est1.ToString();
+
+			lista.Add(est1);
+
+			// RJ
+			est1 = new Estado();
+			est1.Id = 2;
+			est1.Nome = "RJ";
+			lista.Add(est1);
+
+			// MG
+			est1 = new Estado();
+			est1.Id = 3;
+			est1.Nome = "MG";
+			lista.Add(est1);
+
+			//GO
+			est1 = new Estado();
+			est1.Id = 4;
+			est1.Nome = "GO";
+			lista.Add(est1);
+
+			return lista;
+		}
+	}
 	public partial class Form1 : Form  
 	{
 		public Form1()  
@@ -20,7 +63,8 @@ namespace AppWinforms
 
 		private void btnOk_Click(object sender, EventArgs e)
 		{
-			txtResultado.Text = "Benvindos ao Estado do Brasil: " + cboEstados.Text;
+			// seleciona o Item escolhido no comboBox pelo Id. Aparece na caixa de texto o numero do Id apos a string
+			txtResultado.Text = "Benvindos ao Estado do Brasil: " + ((Estado)cboEstados.SelectedItem).Id;
 		}
 
 		private void btnAbre_Click(object sender, EventArgs e)
@@ -33,8 +77,15 @@ namespace AppWinforms
 
 		private void Form1_Load(object sender, EventArgs e) 
 		{
-			// MessageBox.Show("Inicializando o formulario");  
-			                                                
+			// MessageBox.Show("Inicializando o formulario"); 
+
+
+			// Load dos Estados na Lista
+			cboEstados.Items.Clear();
+			foreach (Estado estado in Estado.Lista())
+			{
+				cboEstados.Items.Add(estado);
+			}
 		}
 	}
 }
