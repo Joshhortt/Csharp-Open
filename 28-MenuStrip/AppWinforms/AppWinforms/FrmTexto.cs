8fb741b9f0f46b9bff3207645ec87acec1381b00
Joshhortt <joshhortt@yahoo.com>
2021-02-02 19:05:21 +0000

    Primeiro commit da parte 28 do projeto WinForms. Criar novo formulario com Menu Strip.

---
+++ 28-MenuStrip/AppWinforms/AppWinforms/FrmTexto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	public partial class FrmTexto : Form  // Criado novo Winform
+	{
+		public FrmTexto()
+		{
+			InitializeComponent();
+		}
+
+		private void btnSalvar_Click(object sender, EventArgs e)
+		{
+			SalvarArquivo();  // Colocar tambem o metodo dentro do click do botao
+		}
+
+		private void SalvarArquivo() // Criar um metodo privado sem retorno
+		{
+
+		}
+
+		private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SalvarArquivo();  // Colocar tambem o metodo no clique do botao salvar do Arquivo no Menu Strip
+		}
+	}
+}
---
+++ 28-MenuStrip/AppWinforms/AppWinforms/Program.cs
@@ -0,0 +1,26 @@
+﻿//28. Menu Strip
+//Nesta aula iremos aprender como criar estrutura de menu em nossa aplicação, fazendo um bloco de notas. 
+//Também terá a noção de como é feito programas que exigem serial.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
