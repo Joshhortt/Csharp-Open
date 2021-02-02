7193a9d38d68850a74b703446ef58212471e364d
Joshhortt <joshhortt@yahoo.com>
2021-02-02 17:24:36 +0000

    Copiar de Form1.cs a classe Estado' para classe Estado.cs e mudar propriedades Id' e Nome' para atributos.

---
+++ 26-ComboBox/AppWinforms/AppWinforms/Estado.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWinforms
+{
+	public class Estado
+	{
+		public int Id;     // atributo Id do tipo int
+		public string Nome;   // atributo Nome do tipo string
+
+		public override string ToString()  // override de APPWinforms Estado (fazendo com que nao apareça na textBox
+		{
+			return this.Nome; // retorna o Nome do Estado 
+		}
+
+		public static List<Estado> Lista()  // Criar Lista de Estados com numero Id e Nome
+		{
+			// SP
+			var lista = new List<Estado>();
+			var est1 = new Estado();
+			est1.Id = 1;
+			est1.Nome = "SP";
+
+			var s = est1.ToString();
+
+			lista.Add(est1);
+
+			// RJ
+			est1 = new Estado();
+			est1.Id = 2;
+			est1.Nome = "RJ";
+			lista.Add(est1);
+
+			// MG
+			est1 = new Estado();
+			est1.Id = 3;
+			est1.Nome = "MG";
+			lista.Add(est1);
+
+			//GO
+			est1 = new Estado();
+			est1.Id = 4;
+			est1.Nome = "GO";
+			lista.Add(est1);
+
+			return lista;
+		}
+	}
+}
--- 26-ComboBox/AppWinforms/AppWinforms/Form1.cs
+++ 26-ComboBox/AppWinforms/AppWinforms/Form1.cs
@@ -10,49 +10,6 @@ using System.Windows.Forms;
 
 namespace AppWinforms
 {
-	public class Estado
-	{
-		public int Id;     // propriedade Id do tipo int
-		public string Nome;   // propriedade Nome do tipo string
-
-		public override string ToString()  // override de APPWinforms Estado (fazendo com que nao apareça na textBox
-		{
-			return this.Nome; // retorna o Nome do Estado 
-		}
-
-		public static List<Estado> Lista()  // Criar Lista de Estados com numero Id e Nome
-		{
-			// SP
-			var lista = new List<Estado>();
-			var est1 = new Estado();
-			est1.Id = 1;
-			est1.Nome = "SP";
-
-			var s = est1.ToString();
-
-			lista.Add(est1);
-
-			// RJ
-			est1 = new Estado();
-			est1.Id = 2;
-			est1.Nome = "RJ";
-			lista.Add(est1);
-
-			// MG
-			est1 = new Estado();
-			est1.Id = 3;
-			est1.Nome = "MG";
-			lista.Add(est1);
-
-			//GO
-			est1 = new Estado();
-			est1.Id = 4;
-			est1.Nome = "GO";
-			lista.Add(est1);
-
-			return lista;
-		}
-	}
 	public partial class Form1 : Form  
 	{
 		public Form1()  
