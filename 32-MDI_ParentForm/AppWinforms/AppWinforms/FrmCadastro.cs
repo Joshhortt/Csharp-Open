73c0b0268d3813a515f09c5237381bed25a8555a
Joshhortt <joshhortt@yahoo.com>
2021-02-04 16:48:01 +0000

    Primeiro commit da parte 32 do projeto WinForms.

---
+++ 32-MDI_ParentForm/AppWinforms/AppWinforms/FrmCadastro.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	public partial class FrmCadastro : Form
+	{
+		public FrmCadastro()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnCalcular_Click(object sender, EventArgs e)
+		{
+
+			var nome = textBox1.Text;
+			try
+			{
+				int numero = int.Parse(txtNumero.Text);
+				//numero += 100;
+
+				if(numero == 2)
+				{																												
+				throw new ErroDeProposito("Erro de proposito");
+				}
+				else if(numero == 4)
+				{
+					throw new ErroDeProposito("Erro de proposito generico");
+				}
+
+				numero += 100;  
+
+				MessageBox.Show("Olá " + nome + ", o valor do numero mais 100 é de: " + numero);
+			}
+
+			catch (FormatException errFormat)
+			{
+				MessageBox.Show("Olá cliente, voce por acaso não digitou letras em vez de numeros?" + errFormat);
+				txtNumero.Focus();
+			}
+
+			catch (ErroDeProposito errProposito)
+			{
+				MessageBox.Show("Erro de Proposito" + errProposito.StackTrace);
+			}
+
+			catch (Exception err)  
+			{
+				MessageBox.Show("Olá cliente, voce por acaso não digitou letras em vez de numeros?" + err.Message);
+			}
+			finally
+			{
+				MessageBox.Show("todas as Excepções tratadas");  			                                                
+			}
+		}
+	}
+	public class ErroDeProposito : Exception  
+	{
+		public ErroDeProposito(string erro) : base(erro)  
+		{
+        }
+    }
+}
+ 
+
---
+++ 32-MDI_ParentForm/AppWinforms/AppWinforms/Program.cs
@@ -0,0 +1,27 @@
+﻿//32. MDI Parent Form
+//Nesta aula iremos aprender como organizar os nossos formulários,
+//com MDI Form é possível colocar formulários dentro de formulários, 
+//utilizaremos uma classe singleton para gerenciar a instância.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	static class Programs
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
