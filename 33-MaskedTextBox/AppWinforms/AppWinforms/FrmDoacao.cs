87c80033c2456006faa7cacf1686130418799fc9
Joshhortt <joshhortt@yahoo.com>
2021-02-05 13:41:52 +0000

    Primeiro commit da parte 33 do projeto WinForms.

---
+++ 33-MaskedTextBox/AppWinforms/AppWinforms/FrmDoacao.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics; // adicionar o System.Diagnostics para que o Process.Start execute.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	public partial class FrmDoacao : Form
+	{
+		public FrmDoacao()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Process.Start("https://github.com/Joshhortt/Csharp-Open");  
+		}
+
+		private void FrmDoacao_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ 33-MaskedTextBox/AppWinforms/AppWinforms/Program.cs
@@ -0,0 +1,26 @@
+﻿//33. Masked Text Box
+//Nesta aula iremos aprender como colocar mascara em nossos inputs de dados, 
+//obrigando o nosso usuário digitar de acordo com a mascara que colocarmos em nosso text.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	static class Programs
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false); 
+			Application.Run(MDISingleton.InstanciaMDI());  
+		}
+	}
+}
