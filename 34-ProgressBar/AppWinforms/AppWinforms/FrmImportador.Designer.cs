d5e078669ebe4a17a2ed00f425520ace93d26247
Joshhortt <joshhortt@yahoo.com>
2021-02-05 18:39:53 +0000

    Adicionar novo form 'frmImportador' um metodo 'load' do 'progressBar' e variavel privada.

--- 34-ProgressBar/AppWinforms/AppWinforms/FrmImportador.Designer.cs
+++ 34-ProgressBar/AppWinforms/AppWinforms/FrmImportador.Designer.cs
@@ -29,12 +29,58 @@ namespace AppWinforms
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.progressBar = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnImportar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// progressBar
+			// 
+			this.progressBar.Location = new System.Drawing.Point(71, 108);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(389, 110);
+			this.progressBar.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(66, 65);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(394, 25);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Clique no botao para atualizar os dados";
+			// 
+			// btnImportar
+			// 
+			this.btnImportar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnImportar.Location = new System.Drawing.Point(71, 244);
+			this.btnImportar.Name = "btnImportar";
+			this.btnImportar.Size = new System.Drawing.Size(389, 62);
+			this.btnImportar.TabIndex = 2;
+			this.btnImportar.Text = "Importar";
+			this.btnImportar.UseVisualStyleBackColor = true;
+			// 
+			// FrmImportador
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(540, 450);
+			this.Controls.Add(this.btnImportar);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar);
+			this.Name = "FrmImportador";
 			this.Text = "FrmImportador";
+			this.Load += new System.EventHandler(this.FrmImportador_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ProgressBar progressBar;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnImportar;
 	}
 }
--- 34-ProgressBar/AppWinforms/AppWinforms/FrmImportador.cs
+++ 34-ProgressBar/AppWinforms/AppWinforms/FrmImportador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,25 @@ namespace AppWinforms
 		public FrmImportador()
 		{
 			InitializeComponent();
+		}
+
+		private void FrmImportador_Load(object sender, EventArgs e)
+		{
+
+			//fonte: https://stackoverflow.com/questions/8037070/whats-the-fastest-way-to-read-a-text-file-line-by-line
+			string fileName = (@"D:\arquivos\dados_importados.txt");
+			using (var streamReader = File.OpenText(fileName))
+			{
+				var lines = streamReader.ReadToEnd().Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+				progressBar.Maximum = lines.Length;  //Maximo de linhas
+				progressBar.Minimum = 0;  // Minimo de linhas
+				//foreach (var line in lines)
+				for(var i = 0; i < lines.Length; i++)
+				{
+					var line = lines[i];
+					progressBar.Value = (i + 1);  // adiciona 1 ao zero para que o progress bar comece em 1
+				}
+			}
 		}
 	}
 }
