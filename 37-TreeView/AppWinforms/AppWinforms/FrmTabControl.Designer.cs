aa4df76204ca0e75f9d37bb6eb657daedbdb7af3
Joshhortt <joshhortt@yahoo.com>
2021-02-08 16:24:12 +0000

    Primeiro commit da parte 37 do projeto WinForms.

---
+++ 37-TreeView/AppWinforms/AppWinforms/FrmTabControl.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace AppWinforms
+{
+	partial class FrmTabControl
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.BtnCalcular = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.txtNome = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.btnTab1 = new System.Windows.Forms.Button();
+			this.btnTab2 = new System.Windows.Forms.Button();
+			this.btnTab3 = new System.Windows.Forms.Button();
+			this.tabControl.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl
+			// 
+			this.tabControl.Controls.Add(this.tabPage1);
+			this.tabControl.Controls.Add(this.tabPage2);
+			this.tabControl.Controls.Add(this.tabPage3);
+			this.tabControl.Location = new System.Drawing.Point(12, 39);
+			this.tabControl.Name = "tabControl";
+			this.tabControl.SelectedIndex = 0;
+			this.tabControl.Size = new System.Drawing.Size(841, 444);
+			this.tabControl.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.textBox3);
+			this.tabPage1.Controls.Add(this.label3);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.textBox4);
+			this.tabPage1.Controls.Add(this.label4);
+			this.tabPage1.Controls.Add(this.textBox5);
+			this.tabPage1.Controls.Add(this.label5);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(833, 418);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Dados Basicos";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.button2);
+			this.tabPage2.Controls.Add(this.textBox7);
+			this.tabPage2.Controls.Add(this.label7);
+			this.tabPage2.Controls.Add(this.textBox8);
+			this.tabPage2.Controls.Add(this.label8);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(833, 418);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Dados Essenciais";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.BtnCalcular);
+			this.tabPage3.Controls.Add(this.textBox1);
+			this.tabPage3.Controls.Add(this.txtNome);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(833, 418);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "Dados Pagamentos";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// BtnCalcular
+			// 
+			this.BtnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BtnCalcular.Location = new System.Drawing.Point(231, 197);
+			this.BtnCalcular.Name = "BtnCalcular";
+			this.BtnCalcular.Size = new System.Drawing.Size(432, 39);
+			this.BtnCalcular.TabIndex = 9;
+			this.BtnCalcular.Text = "Calcular";
+			this.BtnCalcular.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(231, 131);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(432, 20);
+			this.textBox1.TabIndex = 6;
+			// 
+			// txtNome
+			// 
+			this.txtNome.AutoSize = true;
+			this.txtNome.Location = new System.Drawing.Point(170, 131);
+			this.txtNome.Name = "txtNome";
+			this.txtNome.Size = new System.Drawing.Size(35, 13);
+			this.txtNome.TabIndex = 5;
+			this.txtNome.Text = "Nome";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(231, 215);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(432, 20);
+			this.textBox3.TabIndex = 18;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(170, 222);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(44, 13);
+			this.label3.TabIndex = 17;
+			this.label3.Text = "Numero";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(231, 283);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(432, 39);
+			this.button1.TabIndex = 16;
+			this.button1.Text = "Calcular";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(231, 153);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(432, 20);
+			this.textBox4.TabIndex = 15;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(170, 160);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(44, 13);
+			this.label4.TabIndex = 14;
+			this.label4.Text = "Numero";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(231, 96);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(432, 20);
+			this.textBox5.TabIndex = 13;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(170, 96);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(35, 13);
+			this.label5.TabIndex = 12;
+			this.label5.Text = "Nome";
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(231, 222);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(432, 39);
+			this.button2.TabIndex = 16;
+			this.button2.Text = "Calcular";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(231, 96);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(432, 20);
+			this.textBox8.TabIndex = 13;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(170, 96);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(35, 13);
+			this.label8.TabIndex = 12;
+			this.label8.Text = "Nome";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(170, 160);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(44, 13);
+			this.label7.TabIndex = 14;
+			this.label7.Text = "Numero";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(231, 153);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(432, 20);
+			this.textBox7.TabIndex = 15;
+			// 
+			// btnTab1
+			// 
+			this.btnTab1.Location = new System.Drawing.Point(19, 485);
+			this.btnTab1.Name = "btnTab1";
+			this.btnTab1.Size = new System.Drawing.Size(75, 23);
+			this.btnTab1.TabIndex = 10;
+			this.btnTab1.Text = "Tab 1";
+			this.btnTab1.UseVisualStyleBackColor = true;
+			this.btnTab1.Click += new System.EventHandler(this.btnTab1_Click);
+			// 
+			// btnTab2
+			// 
+			this.btnTab2.Location = new System.Drawing.Point(121, 485);
+			this.btnTab2.Name = "btnTab2";
+			this.btnTab2.Size = new System.Drawing.Size(75, 23);
+			this.btnTab2.TabIndex = 11;
+			this.btnTab2.Text = "Tab 2";
+			this.btnTab2.UseVisualStyleBackColor = true;
+			this.btnTab2.Click += new System.EventHandler(this.btnTab2_Click);
+			// 
+			// btnTab3
+			// 
+			this.btnTab3.Location = new System.Drawing.Point(231, 485);
+			this.btnTab3.Name = "btnTab3";
+			this.btnTab3.Size = new System.Drawing.Size(75, 23);
+			this.btnTab3.TabIndex = 12;
+			this.btnTab3.Text = "Tab 3";
+			this.btnTab3.UseVisualStyleBackColor = true;
+			this.btnTab3.Click += new System.EventHandler(this.btnTab3_Click);
+			// 
+			// FrmTabControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(865, 510);
+			this.Controls.Add(this.btnTab3);
+			this.Controls.Add(this.tabControl);
+			this.Controls.Add(this.btnTab2);
+			this.Controls.Add(this.btnTab1);
+			this.Name = "FrmTabControl";
+			this.Text = "FrmTabControl";
+			this.Load += new System.EventHandler(this.FrmTabControl_Load);
+			this.tabControl.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage3.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.Button BtnCalcular;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label txtNome;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button btnTab1;
+		private System.Windows.Forms.Button btnTab2;
+		private System.Windows.Forms.Button btnTab3;
+	}
+}
---
+++ 37-TreeView/AppWinforms/AppWinforms/Program.cs
@@ -0,0 +1,25 @@
+﻿//37. TreeView
+// Nesta aula iremos aprender um componente muito utilizado para aplicações Windows Forms e WebForms, 
+// iremos entender para que servem os nodes e como criar em tempo de execução.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppWinforms
+{
+	static class Programs
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false); 
+			Application.Run(MDISingleton.InstanciaMDI());  
+		}
+	}
+}
