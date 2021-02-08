5e28e3914eae4cadfb854e15852cc7f3e9472350
Joshhortt <joshhortt@yahoo.com>
2021-02-08 18:23:46 +0000

    Adicionar metodo de referencia para percorrer TreeView de diretorios no local D: mostrando todas as pastas contidas no formulario.

--- 37-TreeView/AppWinforms/AppWinforms/FrmTree.cs
+++ 37-TreeView/AppWinforms/AppWinforms/FrmTree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,30 @@ namespace AppWinforms
 
 		private void FrmTree_Load(object sender, EventArgs e)
 		{
+			treeView1.Nodes.Clear();  // Limpa todos os nos de forma manual feitos anteriormente.
+			treeView1.Nodes.Add("Scrimba");  // Aqui vou criar o diretorio de raiz Scrimba
+			var node = treeView1.Nodes[0];  // Mando como referencia 
+			loadDiretorios(@"D:\PROGRAMAR\Scrimba", ref node);  // Referencia do metodo criado
 
+		}
+
+		private void loadDiretorios(string diretorio, ref TreeNode node)  // Criar metodo com referencia
+		{
+			string[] arquivos = Directory.GetFiles(diretorio);  // Verifica todos os arquivos que estao dentro desse diretorio que retorna um array de arquivos
+			foreach(string arquivo in arquivos) // Dentro desse array de arquivos eu pego nesse nome de arquivo
+			{
+				node.Nodes.Add(Path.GetFileName(arquivo)); // Dentro do node eu adiciono node e adiciono o nome do arquivo que eu achei la no diretorio
+			}
+
+
+			// Percorrer subdiretórios deste diretório
+			string[] subdiretorios = Directory.GetDirectories(diretorio);
+			foreach(string subdiretorio in subdiretorios)
+			{
+				TreeNode n = new TreeNode(Path.GetFileName(subdiretorio));
+				loadDiretorios(subdiretorio, ref n);
+				node.Nodes.Add(n);
+			}
 		}
 
 		private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
