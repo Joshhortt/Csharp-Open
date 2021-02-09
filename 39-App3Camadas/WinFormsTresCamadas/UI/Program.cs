ec533ba858b0782824d34f781df3753327d66fe7
Joshhortt <joshhortt@yahoo.com>
2021-02-09 16:19:27 +0000

    Refatorar 'AppConfig' e introduzir comentarios em 'program.cs'.

--- 39-App3Camadas/WinFormsTresCamadas/UI/Program.cs
+++ 39-App3Camadas/WinFormsTresCamadas/UI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//39. Aplicação em 3 Camadas
+//Nesta aula iremos aprender como organizar uma aplicação na padrão de arquitetura 3 camadas, 
+//separando cada camada com sua responsabilidade, organizando o seu projeto e pensando em evoluções futuras.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
