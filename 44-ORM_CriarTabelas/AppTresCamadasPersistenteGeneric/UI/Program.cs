31bb5d70cce71b3174b344cb61a74a11e6f2da80
Joshhortt <joshhortt@yahoo.com>
2021-02-18 11:42:25 +0000

    Primeiro commit da parte 44 do projeto ORM. Criar 'Tabelas' de forma dinamica.

---
+++ 44-ORM_CriarTabelas/AppTresCamadasPersistenteGeneric/UI/Program.cs
@@ -0,0 +1,25 @@
+﻿//44. ORM - Object Relational Mapping - Criar Tabelas
+//Nesta aula iremos aprender como criar tabelas na Base de Dados de forma dinamica utilizando tecnicas ORM.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTresCamadasPersistenteGeneric
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
